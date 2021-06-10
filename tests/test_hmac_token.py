1496a1fc6c68ec37432d27f1378f72ca3aaa97cd
jpburbank <jpburbank@gmail.com>
2021-06-09 22:16:36 -0700

    rudimentary unit tests

--- tests/test_hmac_token.py
+++ tests/test_hmac_token.py
@@ -1,5 +1,41 @@
-from hmac_token import __version__
 
+from hmac_token.hmac_token.generate import generate_digest
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_generate_from_empty():
+    signature = generate_digest({})
+    assert isinstance(signature, str)
+    assert len(signature) > 0
+
+def test_generate_from_complex():
+    signature = generate_digest({
+        "id": "MDAwMDAwMDAtMDAwMC0wMDBiLTAxMmMtMDllZGU5NDE2MDAz",
+        "zev": 123445,
+        "name": "bob",
+        "ls": ["asd", "ert", "dfgs"],
+        "aaa": 12.45,
+        "next": {
+            "id": "LTAxMmMtMDllZGU5NDE2MDAzMDAwMDAwMDAtMDAwMC0wMDBi",
+            "name": "luke",
+            "zev": 5551,
+            "aaa": 54.2,
+            "new": False
+        }
+    })
+    assert isinstance(signature, str)
+    assert len(signature) > 0
+
+def test_generate_same():
+    signature_one = generate_digest({"id": 12345})
+    signature_two = generate_digest({"id": 12345})
+    assert signature_one == signature_two
+
+def test_generate_different_from_similar():
+    signature_one = generate_digest({
+        "id": 12345,
+        "name": "bob"
+    })
+    signature_two = generate_digest({
+        "name": "bob",
+        "id": 12345
+    })
+    assert signature_one != signature_two
